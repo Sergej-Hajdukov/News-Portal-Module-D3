44ebcc26a6120fbf9dcd4cdecadf121db96b2988
Sergej-Hajdukov <serha@yandex.ru>
2022-01-10 04:33:23 +0300

    add files to repositiry

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+from datetime import datetime
+
+
+class NewsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['length'] = Post.objects.count()
+        return context
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'news_detail.html'
+    context_object_name = 'news_detail'
